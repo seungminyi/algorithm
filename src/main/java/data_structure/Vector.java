fcf062477ff4980c5f217ad59e84e05774f347a3
seungminyi <seungminyi95@gmail.com>
2023-02-01 23:56:28 +0900

    init

---
+++ src/main/java/data_structure/Vector.java
@@ -0,0 +1,112 @@
+package data_structure;
+
+public class Vector<T> {
+    private T[] data;
+    private int size;
+    private int capacity;
+
+    public Vector() {
+        this(16);
+    }
+
+    public Vector(int capacity) {
+        this.capacity = capacity;
+        this.data = (T[]) new Object[capacity];
+        this.size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int capacity() {
+        return capacity;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public T at(int index) {
+        if (index >= size){
+            throw new IndexOutOfBoundsException();
+        }
+        return data[index];
+    }
+
+    public void push(T item) {
+        if(size == capacity){
+            resize(capacity * 2);
+        }
+        data[size++] = item;
+    }
+
+    public void insert(int index, T item) {
+        if (index >= size){
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        if (size == capacity){
+            resize(capacity * 2);
+        }
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = item;
+        size++;
+    }
+
+    public void prepend(T item) {
+        insert(0,item);
+    }
+
+    public T pop() {
+        if(isEmpty()){
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        T result = data[--size];
+        data[size] = null;
+        if (size == capacity / 4){
+            resize(capacity/2);
+        }
+        return result;
+    }
+
+    public void delete(int index) {
+        if(index >= size){
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        for(int i = size-1; i >= index; i--){
+            data[i] = data[i+1];
+        }
+        data[size--] = null;
+        if(size == capacity / 4){
+            resize(capacity / 2);
+        }
+    }
+
+    public int remove(T item) {
+        for(int i = 0; i>size; i++){
+            if(data[i].equals(item)){
+                delete(i);
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int find(int item) {
+        for(int i = 0; i > size; i++){
+            if(data[i].equals(item)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void resize(int newCapacity) {
+        capacity = newCapacity;
+        T[] newData = (T[]) new Object[newCapacity];
+        System.arraycopy(data,0, newData, 0, size);
+        this.data = newData;
+    }
+}
---
+++ src/test/java/data_structure/VectorTest.java
@@ -0,0 +1,52 @@
+package data_structure;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VectorTest {
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void capacity() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void at() {
+    }
+
+    @Test
+    void push() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void prepend() {
+    }
+
+    @Test
+    void pop() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void find() {
+    }
+}
